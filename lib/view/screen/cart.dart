6a3568e2fbdb1e9a975565835855af711ef2e4d0
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-02-01 20:36:15 +0200

    97

--- lib/view/screen/cart.dart
+++ lib/view/screen/cart.dart
@@ -1,3 +1,4 @@
+import 'package:ecommercecourse/core/constant/imageasset.dart';
 import 'package:flutter/material.dart';
 
 class Cart extends StatelessWidget {
@@ -7,13 +8,32 @@ class Cart extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(
-        title:Text("Title"),
+        title:const Text("Title"),
       ),
       body:ListView(children: [
         Container(
           alignment: Alignment.center,
-          child:Text("Cart",style:TextStyle(
+          child:const Text("Cart",style:TextStyle(
               fontSize: 25,fontWeight:FontWeight.bold),),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Card(child:Row(children: [
+                Expanded(flex: 2,child:Image.asset(AppImageAsset.logo)),
+                
+                const Expanded(flex:3,child:ListTile(title:Text("Macbook M2 Max"),
+                subtitle:Text("300.00 \$",style:TextStyle(color:Colors.red,fontSize:15),),)),
+                 
+                 Expanded(child:Column(children: [
+                  IconButton(onPressed:(){}, icon:const Icon(Icons.add)),
+                  Container(child:Text("2",style:TextStyle(fontFamily:"sans"),),),
+                  IconButton(onPressed:(){}, icon:const Icon(Icons.remove)),
+                ],)),
+              ],),),
+            ],
+          ),
         )
       ],),
     );
