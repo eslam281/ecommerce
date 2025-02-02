62f0104b1e625b0158475636396e71d0b1a18049
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-02-02 14:19:25 +0200

    98

--- lib/view/screen/cart.dart
+++ lib/view/screen/cart.dart
@@ -1,3 +1,4 @@
+import 'package:ecommercecourse/core/constant/color.dart';
 import 'package:ecommercecourse/core/constant/imageasset.dart';
 import 'package:ecommercecourse/view/widget/cart/buttoncart.dart';
 import 'package:flutter/material.dart';
@@ -12,15 +13,27 @@ class Cart extends StatelessWidget {
       floatingActionButton:Column(
         mainAxisSize:MainAxisSize.min,
         children: [
-          const Row(mainAxisAlignment:MainAxisAlignment.spaceAround,
+          const Row(mainAxisAlignment:MainAxisAlignment.spaceBetween,
             children: [
-            Text("price",style:TextStyle(fontSize: 16),),
-            Text("1200 \$",style:TextStyle(fontSize: 16)),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:20),
+              child: Text("price",style:TextStyle(fontSize: 16),),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:20),
+              child: Text("1200 \$",style:TextStyle(fontSize: 16)),
+            ),
           ],),
-          const Row(mainAxisAlignment:MainAxisAlignment.spaceAround,
+          const Row(mainAxisAlignment:MainAxisAlignment.spaceBetween,
             children: [
-            Text("Shpping",style:TextStyle(fontSize: 16),),
-            Text("1200 \$",style:TextStyle(fontSize: 16)),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:20),
+              child: Text("Shpping",style:TextStyle(fontSize: 16),),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:20),
+              child: Text("1200 \$",style:TextStyle(fontSize: 16)),
+            ),
           ],),
 
           const Padding(
@@ -28,10 +41,16 @@ class Cart extends StatelessWidget {
             child: Divider(color:Colors.black,),
           ),
 
-          const Row(mainAxisAlignment:MainAxisAlignment.spaceAround,
+          const Row(mainAxisAlignment:MainAxisAlignment.spaceBetween,
             children: [
-            Text("Total Price",style:TextStyle(fontSize: 16),),
-            Text("1200 \$",style:TextStyle(fontSize: 16)),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:20),
+              child: Text("Total Price",style:TextStyle(color:Colors.red,fontSize:18),),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal:20),
+              child: Text("2400 \$",style:TextStyle(color:Colors.red,fontSize:18)),
+            ),
           ],),
           CustomButtonCart(textbutton: "Place order", onPressed: (){})
       ],),
@@ -56,6 +75,14 @@ class Cart extends StatelessWidget {
             ],
           ),
         ),
+        SizedBox(height: 10),
+        Container(
+          padding:EdgeInsets.only(bottom:5),
+          margin:EdgeInsets.symmetric(horizontal:20),
+          decoration:BoxDecoration(color:AppColor.primaryColor,
+          borderRadius:BorderRadius.circular(20)),
+          child:Text('you have 2 Items in your List',textAlign:TextAlign.center,
+          style:TextStyle(color:Colors.white,fontSize:20,wordSpacing:2),),),
         Padding(
           padding: const EdgeInsets.all(10),
           child: Column(
