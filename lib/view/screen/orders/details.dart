64080c2b950ed69881e42a6f22cbcf9c2ee3b8cc
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-02-17 14:31:19 +0200

    160

--- lib/view/screen/orders/details.dart
+++ lib/view/screen/orders/details.dart
@@ -1,3 +1,4 @@
+import 'package:ecommercecourse/core/class/handlingdataview.dart';
 import 'package:ecommercecourse/core/constant/color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,56 +10,63 @@ class OrdersDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    OrdersDetailsController controller = Get.put(OrdersDetailsController());
+    Get.put(OrdersDetailsController());
     return Scaffold(
       appBar:AppBar(title:const Text("Orders details"),),
-      body:Container(
-        padding:const EdgeInsets.all(20),
-        child:ListView(children: [
-          const SizedBox(height: 20,),
-        Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical:8),
-              child: Table(
-                border: TableBorder.all(borderRadius:BorderRadius.circular(10)),
-                children: const [
-                TableRow(children:[
-                  Text("Item",textAlign:TextAlign.center,style:TextStyle(fontWeight:FontWeight.bold,fontSize: 20),),
-                  Text("QTY",textAlign:TextAlign.center,style:TextStyle(fontWeight:FontWeight.bold,fontSize: 20)),
-                  Text("Price",textAlign:TextAlign.center,style:TextStyle(fontWeight:FontWeight.bold,fontSize: 20)),
-                ]),
-                TableRow(children:[
-                  Text("adf",textAlign:TextAlign.center,),
-                  Text("4",textAlign:TextAlign.center),
-                  Text("400",textAlign:TextAlign.center),
-                ]),
-                TableRow(children:[
-                  Text("Itgfhfkem",textAlign:TextAlign.center,),
-                  Text("1",textAlign:TextAlign.center),
-                  Text("200",textAlign:TextAlign.center),
-                ]),
-              ],),
-            ),
-            const SizedBox(height: 10,),
-            const Card(
-              child: Padding(padding:EdgeInsets.symmetric(horizontal: 20,vertical: 5),
-                child: Text("Price :  2400 \$",textAlign:TextAlign.center,style:TextStyle(
-                    fontSize:20,color:AppColor.primaryColor,fontWeight:FontWeight.bold
-                ),),
+      body:GetBuilder<OrdersDetailsController>(
+        builder: (controller) {
+          return Container(
+            padding:const EdgeInsets.all(20),
+            child:HandlingDataView(
+              statusRequest:controller.statusRequest,
+              widget: ListView(children: [
+                const SizedBox(height: 20,),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical:8),
+                    child: Table(
+                      border: TableBorder.all(borderRadius:BorderRadius.circular(10)),
+                      children: [
+                      const TableRow(children:[
+                        Text("Item",textAlign:TextAlign.center,style:TextStyle(fontWeight:FontWeight.bold,fontSize: 20),),
+                        Text("QTY",textAlign:TextAlign.center,style:TextStyle(fontWeight:FontWeight.bold,fontSize: 20)),
+                        Text("Price",textAlign:TextAlign.center,style:TextStyle(fontWeight:FontWeight.bold,fontSize: 20)),
+                      ]),
+                      ...List.generate(
+                        controller.data.length,
+                      (index) =>
+                          TableRow(children:[
+                            Text("${controller.data[index].itemsName}",textAlign:TextAlign.center,),
+                            Text("${controller.data[index].countitems}",textAlign:TextAlign.center),
+                            Text("${controller.data[index].itemsprice}",textAlign:TextAlign.center),
+                          ])
+                        )
+
+                    ],),
+                  ),
+                  const SizedBox(height: 10,),
+                   Card(
+                    child: Padding(padding:const EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                      child: Text("Total Price :  ${controller.ordersModel.ordersTotalprice} \$",textAlign:TextAlign.center,style:const TextStyle(
+                          fontSize:20,color:AppColor.primaryColor,fontWeight:FontWeight.bold
+                      ),),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-          const SizedBox(height: 20,),
-           if(controller.ordersModel.ordersType==0)
-           Card(child: ListTile(
-            title:Text("${controller.ordersModel.addressName?.toUpperCase()}",style:const TextStyle(
-                fontSize:20,color:AppColor.primaryColor,fontWeight:FontWeight.bold)),
-            subtitle:Text("${controller.ordersModel.addressCity} // ${controller.ordersModel.addressStreet}",style:
-            const TextStyle(fontSize:17)),
-          ),)
-      ],),),
+                const SizedBox(height: 20,),
+                 if(controller.ordersModel.ordersType==0)
+                 Card(child: ListTile(
+                  title:Text("${controller.ordersModel.addressName?.toUpperCase()}",style:const TextStyle(
+                      fontSize:20,color:AppColor.primaryColor,fontWeight:FontWeight.bold)),
+                  subtitle:Text("${controller.ordersModel.addressCity} // ${controller.ordersModel.addressStreet}",style:
+                  const TextStyle(fontSize:17)),
+                ),)
+                        ],),
+            ),);
+        }
+      ),
     );
   }
 }
